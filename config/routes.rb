dae2101be2f47360351d70ee1515e9fa6316548e
Luiz Neiva <luizgzn@gmail.com>
2018-01-23 19:01:33 -0200

    Refactor nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-
-  get 'rooms/show'
-
-  get 'bookings/index'
-
-  get 'bookings/show'
-
-  get 'motels/show'
-
-  get 'motels/index'
-
   devise_for :users
   resources :motels, only: [:index, :show] do
     resources :rooms, only: [:index, :show]
