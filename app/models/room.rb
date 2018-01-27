6289563f19f75499a614e55efb5059e0dd7d6678
Luiz Neiva <luizgzn@gmail.com>
2018-01-27 01:09:51 -0200

    User has one motel

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ActiveResource::Base
   self.site = "https://luvotels-hiring-api.herokuapp.com/motels/:motel_id/"
   self.element_name = "rooms"
-  has_many :bookings
   belongs_to :motel
+  has_many :bookings
 end
