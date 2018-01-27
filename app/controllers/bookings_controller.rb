00eafa0655fc542a44f1aaafaa00d737475233de
Luiz Neiva <luizgzn@gmail.com>
2018-01-27 01:04:26 -0200

    Simplify logic + more data crunch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,26 @@
 class BookingsController < ApplicationController
   
   before_action :set_bookings, only: [:index, :show]
-  include BookingsHelper
   
   def index
-    # byebug
+    @revenue_total = @bookings.map {|id| id["amount_centavos"] }.sum/100
+    @fee_total = @bookings.map { |i| i.values[2]/100 * i.values[3]/100 }.inject(:+)
+    @income_total = @revenue_total - @fee_total
+    @fee_pp_avg_total = @bookings.map {|id| id["fee_percentage"] }.inject{ |sum, el| sum + el }.to_f / @bookings.size
   end
 
   def show
+    @bookings_room = @bookings.select {|id| id["room_id"] == params[:format].to_i }
+    @revenue_room = @bookings_room.map {|id| id["amount_centavos"] }.sum/100
+    @fee_pp_avg_room = @bookings_room.map {|id| id["fee_percentage"] }.inject{ |sum, el| sum + el }.to_f / @bookings_room.size
   end
-
+  
   private
-
+  
   def set_bookings
     @motel_id = current_user.motel_id
-    @bookings_all = Booking.find(:all, :from => "/motels/#{current_user.motel_id}/bookings.json").map(&:attributes)
-    @bookings_room = @bookings_all.select {|id| id["room_id"] == params[:format].to_i }
-    @revenue_total = @bookings_all.map {|id| id["amount_centavos"] }.sum/100
-    @revenue_room = @bookings_room.map {|id| id["amount_centavos"] }.sum/100
-    @fee_percentage_average_room = @bookings_room.map {|id| id["fee_percentage"] }.inject{ |sum, el| sum + el }.to_f / @bookings_room.size
-    @fee_percentage_average_total = @bookings_all.map {|id| id["fee_percentage"] }.inject{ |sum, el| sum + el }.to_f / @bookings_all.size
+    @bookings = Booking.find(:all, :from => "/motels/#{@motel_id}/bookings.json").map(&:attributes)
+    @motels = Motel.all.select { |motel| motel.id == current_user.motel_id }
+    @rooms = Room.find(:all, :params => {motel_id: @motel_id})
   end
 end
