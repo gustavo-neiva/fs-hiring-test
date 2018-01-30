49b9961e11208aa2a47b6864a5acfca9ef116bef
Luiz Neiva <luizgzn@gmail.com>
2018-01-30 18:23:05 -0200

    Add graph with revenue grouped by month

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,23 +11,30 @@ class BookingsController < ApplicationController
     @fee_pp_avg_total = @bookings.map {|id| id["fee_percentage"] }.inject{ |sum, el| sum + el }.to_f / @bookings.size
     @bookings_refactor = @bookings.each { |el| el["amount_centavos"] = el["amount_centavos"]/100 }.each { |el| el["paid_at"] = el["paid_at"].to_datetime.strftime("%b, %m %Y - %H:%M") }.each { |el| el["created_at"] = el["created_at"].to_datetime.strftime("%b, %m %Y - %H:%M") } 
     @bookings_json = @bookings_refactor.to_json
-    @rooms_id = @rooms.map(&:attributes).map { |el| el[:id]}
   end
-
+  
   def show
     @bookings_room = @bookings.select {|id| id["room_id"] == params[:id].to_i }
+    @room = @rooms_arr.select {|id| id["id"] == params[:id].to_i }
     @revenue_room = @bookings_room.map {|id| id["amount_centavos"] }.sum/100
+    @fee_room = @bookings_room.map { |i| i.values[2]/100 * i.values[3]/100 }.inject(:+)
+    @income_room = @revenue_room - @fee_room
     @fee_pp_avg_room = @bookings_room.map {|id| id["fee_percentage"] }.inject{ |sum, el| sum + el }.to_f / @bookings_room.size
+    @sorted_by_month = (@bookings_room.map { |el| el.fetch("paid_at")}).zip((@bookings_room.map { |el| el.fetch("amount_centavos")})).group_by_month { |u| u[0] }
+    @total_rev_month = @sorted_by_month.map { |k,v| [k, (v.map(&:last).sum)/100]}
+    console
   end
   
   private
-
+  
   def set_bookings
     @motel_id = current_user.motel_id
     @bookings_obj = Booking.where(motel_id: @motel_id)
     @bookings = @bookings_obj.map(&:attributes)
     @motel = Motel.all.select { |motel| motel.id == current_user.motel_id }
     @rooms = Room.where(motel_id: @motel_id)
+    @rooms_arr = @rooms.map(&:attributes)
+    @rooms_id = @rooms_arr.map { |el| el[:id]}
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,21 @@
 module ApplicationHelper
+  
   def number_to_currency_br(number)
     number_to_currency(number, :unit => "R$ ", :separator => ",", :delimiter => ".")
   end
+
   def data_fetch(data_set, key)
     data_set.map { |el| el.fetch(key)}
   end
-  def data_graph (array1, array2)
-    Hash[array1.zip(array2)]
-  end
+
   def date_time(array)
     array.map { |el| el.to_datetime}.sort.map { |date| date.strftime("%b, %m %Y - %H:%M") }
   end
+
+  def data_graph (array1, array2)
+    Hash[array1.zip(array2)]
+  end
+
   def room_revenue(bookings, rooms, rooms_name)
     index = 0
     names = []
@@ -22,4 +27,5 @@ module ApplicationHelper
     end
     Hash[names.zip result]
   end
+  
 end
